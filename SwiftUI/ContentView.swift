6aef8a3c75dccf180becdcfd8945ebe4aa347b29
jonysingla <jony.singla19@gmail.com>
2020-09-15 12:05:49 +0530

    Initial Commit

---
+++ SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by Jony on 03/09/20.
+//  Copyright © 2020 Jony. All rights reserved.
+//
+
+//import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
